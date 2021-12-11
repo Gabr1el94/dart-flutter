a82b922f55103194a8f20bc87375d2de5c10b243
Gabr1el94 <bielsystem64@gmail.com>
2021-12-11 16:31:09 -0300

    understading about the variables

---
+++ dart/variables.dart
@@ -0,0 +1,19 @@
+void main(){
+    /* Types datas in the variables*/
+    String name = "GabSilva";
+    int bananas = 5;
+    double price = 10.50;
+    bool isActive = true; // true or false
+    
+    print("The name store is: "+ name);
+    print("The $name has $bananas bananas");
+
+    var test = 1;
+    print(test);
+    
+    dynamic test2 = 3;
+    print(test2);
+    test2="Testing in Dart";
+    print(test2);
+    
+}
