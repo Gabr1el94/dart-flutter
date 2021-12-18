e74e2f516de39881fec903d8fed9c877f901e57f
Gabr1el94 <bielsystem64@gmail.com>
2021-12-18 15:33:08 -0300

    static, final and const

---
+++ dart/modifiers.dart
@@ -0,0 +1,18 @@
+class Valores{
+    
+    static int vezesClicado;
+
+}
+
+class Pessoa {
+    
+}
+
+void main() {
+
+    Valores.vezesClicado = 2;
+
+    const numero = 3;
+
+    final Pessoa people = Pessoa();
+}
